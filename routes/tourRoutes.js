46e197351fa2d1fc59723810838619ef56f9efd8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-12-04 16:22:15 +0530

    Added environment variables and changed script

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const tourControllers = require('../controllers/tourControllers');
 
-const { getTours, createTour, getTour, updateTour, deleteTour } =
-  tourControllers;
+const {
+  getTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkID,
+  checkBody,
+} = tourControllers;
 
 const router = express.Router();
 
-router.param('id', tourControllers.checkID);
+router.param('id', checkID);
 
-router.route('/').get(getTours).post(createTour);
+router.route('/').get(getTours).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
